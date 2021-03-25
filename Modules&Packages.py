610bcd7c6b2e1682a5594a1947b969a61a6b35e8
MiltonSimba <you@example.com>
2021-03-25 14:20:51 +0200

    Finishing Py Tutorial &Excel Automation Exercise

--- Modules&Packages.py
+++ Modules&Packages.py
@@ -1,3 +1,6 @@
+#  MODULES ~ PACKAGES ~ DIRECTORIES ~ FILES
+
+
 # Modules are used to organise code in a program making reusable too
 
 import converter   #here we are importing code in the lists module without .py
@@ -52,3 +55,57 @@ from ecommerce.shipping import calc_shipping  #you can add more functions by usi
 from ecommerce import shipping
 shipping.calc_shipping
 
+
+##################### Generating Random Numbers ###########################
+#google Python 3 packages index and open link with doc.python
+#select random numbers package
+
+import random       #no need to specify module or package because its a built in function
+
+
+for i in range (3):
+    print (random.random())                        #picks a default number between 0 and 1
+    print(random.randint(10,20))                   #picks a selected range of number betwen in this case 10 and 20
+
+
+members = ['John', 'Mary', 'Bob', 'Mosh']
+leader = random.choice (members)
+print (leader)                                      #picks a random item in the list
+
+
+
+######################### Exercise ########################
+# Write a program that predicts a dice number
+
+# Create a class called Dice
+# it should have a method called roll() and everytime we call this method we should get a tuple
+ 
+import random
+
+
+class Dice:
+    def roll(self):
+        first = random.randint (1,6)
+        last = random.randint (1,6)
+        return first, last                            #in python you dont have to return a tuple in parentheses once its not in cluded in () python knows its a tuple
+
+
+dice1 = Dice ()
+print (dice1.roll())
+
+
+########################## Files and Directories #####################################
+from pathlib import Path                    #built in package and class NB uppercase P in Path
+
+# we can use:
+# 1. Absolute path eg c:\Program Files\Microsoft
+# 2. Relative Path
+
+path = Path ()              #class Path checks current directory for entered argument file namme or directory
+#print (path.exists())                 # returns boolean for existance of a directory
+#print (path.mkdir())                   #makes directory
+#print (path.rmdir())                   #removes directory
+
+for file in path.glob ('*.py'):
+    print (file)                            #generates a list of files with the .py extension 
+    
